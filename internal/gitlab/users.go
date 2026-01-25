19c141eeb809f78d4d82ee6f531b69e023dc8be2
xMoelletschi <98024478+xMoelletschi@users.noreply.github.com>
2026-01-25 13:02:06 +0100

    feat(gitlab): implement API client with pagination

--- internal/gitlab/users.go
+++ internal/gitlab/users.go
@@ -1,9 +1,35 @@
 package gitlab
 
-type User struct {
-	ID    int
-	Name  string
-	Email string
-}
+import (
+	"context"
+	"fmt"
 
-func (c *Client) ListUsers() ([]User, error) {}
+	gl "gitlab.com/gitlab-org/api/client-go"
+)
+
+func (c *Client) ListUsers(ctx context.Context) ([]*gl.User, error) {
+	var allUsers []*gl.User
+
+	opts := &gl.ListUsersOptions{
+		ListOptions: gl.ListOptions{
+			Page:    1,
+			PerPage: 100,
+		},
+	}
+
+	for {
+		users, resp, err := c.api.Users.ListUsers(opts, gl.WithContext(ctx))
+		if err != nil {
+			return nil, fmt.Errorf("listing users: %w", err)
+		}
+
+		allUsers = append(allUsers, users...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	return allUsers, nil
+}
