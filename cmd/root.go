cf7d1412da368a1ce94342fa10f3842a4ebf2908
xMoelletschi <98024478+xMoelletschi@users.noreply.github.com>
2026-02-02 21:49:30 +0100

    feat(logging): integrate structured logging with slog

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,6 +11,8 @@ var (
 	terraformDir string
 	gitlabToken  string
 	gitlabURL    string
+	verbose      bool
+	jsonOutput   bool
 )
 
 var rootCmd = &cobra.Command{
@@ -25,7 +28,29 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initLogger)
+
 	rootCmd.PersistentFlags().StringVar(&terraformDir, "terraform-dir", ".", "Path to Terraform directory")
 	rootCmd.PersistentFlags().StringVar(&gitlabToken, "gitlab-token", "", "GitLab API token (or set GITLAB_TOKEN env var)")
 	rootCmd.PersistentFlags().StringVar(&gitlabURL, "gitlab-url", "https://gitlab.com", "GitLab instance URL")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose (debug) logging")
+	rootCmd.PersistentFlags().BoolVar(&jsonOutput, "json", false, "Output logs in JSON format (useful for CI)")
+}
+
+func initLogger() {
+	level := slog.LevelInfo
+	if verbose {
+		level = slog.LevelDebug
+	}
+
+	opts := &slog.HandlerOptions{Level: level}
+
+	var handler slog.Handler
+	if jsonOutput {
+		handler = slog.NewJSONHandler(os.Stderr, opts)
+	} else {
+		handler = slog.NewTextHandler(os.Stderr, opts)
+	}
+
+	slog.SetDefault(slog.New(handler))
 }
