9cc253efa4ae998fc4ccece26b8608922a372791
xMoelletschi <98024478+xMoelletschi@users.noreply.github.com>
2026-02-08 12:16:35 +0100

    feat(gitlab): add --group flag to scope scan to a top-level group

--- internal/gitlab/groups.go
+++ internal/gitlab/groups.go
@@ -10,25 +10,42 @@ import (
 func (c *Client) ListGroups(ctx context.Context) ([]*gl.Group, error) {
 	var allGroups []*gl.Group
 
+	if c.group != "" {
+		opts := &gl.ListDescendantGroupsOptions{
+			ListOptions: gl.ListOptions{
+				Page:    1,
+				PerPage: 100,
+			},
+		}
+		for {
+			groups, resp, err := c.api.Groups.ListDescendantGroups(c.group, opts, gl.WithContext(ctx))
+			if err != nil {
+				return nil, fmt.Errorf("listing descendant groups: %w", err)
+			}
+			allGroups = append(allGroups, groups...)
+			if resp.NextPage == 0 {
+				break
+			}
+			opts.Page = resp.NextPage
+		}
+		return allGroups, nil
+	}
+
 	opts := &gl.ListGroupsOptions{
 		ListOptions: gl.ListOptions{
 			Page:    1,
 			PerPage: 100,
 		},
 	}
-
 	for {
 		groups, resp, err := c.api.Groups.ListGroups(opts, gl.WithContext(ctx))
 		if err != nil {
 			return nil, fmt.Errorf("listing groups: %w", err)
 		}
-
 		allGroups = append(allGroups, groups...)
-
 		if resp.NextPage == 0 {
 			break
 		}
-
 		opts.Page = resp.NextPage
 	}
 	return allGroups, nil
--- internal/gitlab/projects.go
+++ internal/gitlab/projects.go
@@ -10,26 +10,44 @@ import (
 func (c *Client) ListProjects(ctx context.Context) ([]*gl.Project, error) {
 	var allProjects []*gl.Project
 
+	if c.group != "" {
+		opts := &gl.ListGroupProjectsOptions{
+			ListOptions: gl.ListOptions{
+				Page:    1,
+				PerPage: 100,
+			},
+			IncludeSubGroups: gl.Ptr(true),
+		}
+		for {
+			projects, resp, err := c.api.Groups.ListGroupProjects(c.group, opts, gl.WithContext(ctx))
+			if err != nil {
+				return nil, fmt.Errorf("listing group projects: %w", err)
+			}
+			allProjects = append(allProjects, projects...)
+			if resp.NextPage == 0 {
+				break
+			}
+			opts.Page = resp.NextPage
+		}
+		return allProjects, nil
+	}
+
 	opts := &gl.ListProjectsOptions{
 		ListOptions: gl.ListOptions{
 			Page:    1,
 			PerPage: 100,
 		},
 	}
-
 	for {
 		projects, resp, err := c.api.Projects.ListProjects(opts, gl.WithContext(ctx))
 		if err != nil {
 			return nil, fmt.Errorf("listing projects: %w", err)
 		}
-
 		allProjects = append(allProjects, projects...)
-
 		if resp.NextPage == 0 {
 			break
 		}
 		opts.Page = resp.NextPage
 	}
-
 	return allProjects, nil
 }
